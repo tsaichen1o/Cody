3950becd5c1ae50a6aae9608ab64908bff99ce4d
羅采蓁 <b09705059@ntu.edu.tw>
2024-02-27 23:52:53 +0800

    Create "23_Picking_Numbers.cpp"

---
+++ HackerRank/Algorithms/23_Picking_Numbers.cpp
@@ -0,0 +1,115 @@
+/**
+ * @file 23_Picking_Numbers.cpp
+ * @author auntieyafen (b09705059@gmail.com)
+ * @brief Prepare > Algorithms > Implementation > Picking Numbers
+ * @version 0.1
+ * @date 2024-02-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+int pickingNumbers(vector<int> a) {
+    int cand2[3] = {6,6,6};
+    int count[100]{};
+    int cand1[3] = {5,5,5};
+    int maxCount = 0, tempCount = 0;
+    
+    for (int num : a) {
+        count[num]++;
+    }
+    for (int i = 1; i < 100; i++) {
+        tempCount = count[i] + count[i - 1];
+        if (tempCount > maxCount) {
+            maxCount = tempCount;
+        }
+    }
+    cout << count[100];
+    
+    return maxCount;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = pickingNumbers(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
